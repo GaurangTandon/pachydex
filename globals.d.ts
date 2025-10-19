9fbb68449716c59d28ed40ffc74a097fda03fa3e
Gaurang Tandon <1gaurangtandon@gmail.com>
2025-10-19 17:35:35 +0530

    first batch

---
+++ globals.d.ts
@@ -0,0 +1,1 @@
+declare let chrome: import('@types/chrome');
---
+++ vite.config.js
@@ -0,0 +1,25 @@
+// For building the huggingface files so they can be loaded from a local file on the filesystem
+// as Chrome extension service worker cannot directly import from a module file
+
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+  build: {
+    outDir: "logger/built",
+
+    lib: {
+      entry: resolve(__dirname, "logger/hugging.js"),
+      fileName: (format) => `hugging.js`,
+
+      // Won't minify even after specifying esbuild options
+      // https://github.com/vitejs/vite/issues/6079
+      // Doesn't make a difference of move than 1kB
+      // So ignoring as it's locally downloaded either way
+      formats: ["es"],
+    },
+    sourcemap: true,
+    minify: "esbuild",
+    target: "esnext",
+  },
+});
